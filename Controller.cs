80c96329bae63804e25c2fb74c110482489731be
Nolankicks <nolandimensions@gmail.com>
2024-03-15 21:24:51 -0400

    Refactor controller update and camera position calculation

--- Controller.cs
+++ Controller.cs
@@ -9,15 +9,19 @@ public sealed class Controller : Component
 	[Property] public GameObject Body { get; set; }
 	[Property] public CameraComponent Camera;
 	public Angles EyeAngles { get; set; }
-	protected override void OnUpdate()
+	protected override void OnFixedUpdate()
 	{
 		BuildMoveAngles();
 		CamRot();
 		UpdateCamPos();
 		Log.Info(WishVelocity);
-		Rigidbody.Velocity += Vector3.Zero.WithAcceleration(WishVelocity, 10f);
+		Rigidbody.ApplyForce(WishVelocity * 500);
 
 		//Camera.Transform.Rotation = EyeAngles.ToRotation();
+	}
+	protected override void OnUpdate()
+	{
+		
 	}
 	void BuildMoveAngles()
 	{
@@ -27,23 +31,22 @@ public sealed class Controller : Component
 			WishVelocity = new Angles(0, EyeAngles.yaw, 0).ToRotation() * WishVelocity;
 			WishVelocity.WithZ(0);
 			WishVelocity.ClampLength(1);
-			WishVelocity *= 5;
+			WishVelocity *= 700;
 		}
 	}
 	void CamRot()
 	{
 		var e = EyeAngles;
-		e += Input.AnalogLook * Preferences.Sensitivity;
+		e += Input.AnalogLook * 5;
 		e.pitch = e.pitch.Clamp(-89, 89);
 		EyeAngles = e;
 	}
 
-	void UpdateCamPos()
-	{
-		var lookDir = EyeAngles.ToRotation();
-		var targetpos = Transform.Position.LerpTo(Body.Transform.Position + lookDir.Backward * 300 + Vector3.Up * 75.0f, 25f);
-		Camera.Transform.Position = targetpos;
-		targetpos.z = Camera.Transform.Position.z.LerpTo(targetpos.z, RealTime.Delta * 25.0f);
-		Camera.Transform.Rotation = lookDir;
-	}
+void UpdateCamPos()
+    {	var center = Rigidbody.PhysicsBody.GetBounds().Center;
+        var lookDir = EyeAngles.ToRotation();
+        var targetpos = Transform.Position.LerpTo(center + lookDir.Backward * 300 + Vector3.Up * 75.0f, 1f);
+        Camera.Transform.Position = targetpos;
+        Camera.Transform.Rotation = lookDir;
+    }
 }
